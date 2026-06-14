1e685b2fb98b9a3a1682be556ebf3331eaabe09c
Richie McIlroy <33632126+richiemcilroy@users.noreply.github.com>
2026-06-14 03:43:53 +0100

    feat(player): export telemetry surface from package index

--- packages/player/src/index.ts
+++ packages/player/src/index.ts
@@ -1,8 +1,16 @@
 export { RendPlayer } from "./rend-player";
+export { REND_PLAYER_VERSION } from "./telemetry";
 export type {
   PlaybackBootstrapReady,
   PlaybackBootstrapResponse,
   PlaybackPrefetchHint,
   RendPlayerProps,
   RendPlayerState,
+  RendPlayerTimings,
 } from "./rend-player";
+export type {
+  RendPlayerPlaybackMode,
+  RendPlayerTelemetryEvent,
+  RendPlayerTelemetryInput,
+  RendPlayerTelemetryPhase,
+} from "./telemetry";
